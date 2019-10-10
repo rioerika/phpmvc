d78b4ec48f89e23ce7a090a2380222e337459a44
Rio Erika <rio_173040041@mail.unpas.ac.id>
2019-10-10 20:51:15 +0700

    Membuat fungsi untuk menangkap data dari url di file App.php

---
+++ app/core/App.php
@@ -0,0 +1,13 @@
+<?php 
+
+class App{
+	public function __construct(){
+		$url = $this->parseURL();
+		var_dump($url);
+	}
+
+	public function parseURL(){
+		if(isset($_GET['url']));
+		return $url;
+	}
+}
